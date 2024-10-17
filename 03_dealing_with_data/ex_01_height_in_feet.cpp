e7370fdb2b5840003af186df9bae5ae81b1c6403
Marina Novosad <zmeika.marina@gmail.com>
2024-10-17 12:44:02 +0000

    Chapter 03. Exercise 01. Height in feet

---
+++ 03_dealing_with_data/ex_01_height_in_feet.cpp
@@ -0,0 +1,19 @@
+// Write a short program that asks for your height in integer inches and then converts
+// your height to feet and inches. Have the program use the underscore character to
+// indicate where to type the response. Also use a const symbolic constant to represent the 
+// conversion factor.
+#include <iostream>
+
+using namespace std;
+
+const unsigned short InchInFeet = 12;
+
+int main()
+{
+    unsigned short inches;
+    cout << "Enter your height in inches:_____\b\b\b\b\b";
+    cin >> inches;
+    cout << "Your height in feet = " << inches / InchInFeet << endl;
+    
+    return 0;
+}
