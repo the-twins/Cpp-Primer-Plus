c0903c63c4dae626d8eed269f82cc4a2678b47a7
Marina Novosad <zmeika.marina@gmail.com>
2024-10-18 12:23:00 +0000

    Chapter 03. Exercise 06. Gasoline.

---
+++ 03_dealing_with_data/ex_06_gasoline.cpp
@@ -0,0 +1,23 @@
+// Write a program that asks how many miles you have driven and how many gallons
+// of gasoline you have used and then reports the miles per gallon your car has gotten.
+// Or, if you prefer, the program can request distance in kilometers and petrol in liters
+// and then report the result European style, in liters per 100 kilometers.
+#include <iostream>
+
+using namespace std;
+
+const float Km = 100.0;
+
+int main()
+{
+    float distance_km;
+    float liters;
+    cout << "Enter distance in kilometers: ";
+    cin >> distance_km;
+    cout << "Enter how many liters gasoline you have used: ";
+    cin >> liters;
+    cout << "Gasoline consumption per 100 km is " << Km * liters / distance_km << endl;
+
+    return 0;
+}
+    
